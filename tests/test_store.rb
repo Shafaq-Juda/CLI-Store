ed4fe89c3af68fe57d0027068ba15a723b944e38
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-02-02 07:05:14 +0000

    Created minitest class

--- tests/test_store.rb
+++ tests/test_store.rb
@@ -4,6 +4,7 @@ require './app'
 class TestApp < Minitest::Test
   def test_application
     application = App.new
-    assert_instance_of Store, App.new.run, "App method failed"
+    assert_equal "Invalid option! Please try again.", application.select_department(8), "Select Department method failed"           
   end
 end
+
